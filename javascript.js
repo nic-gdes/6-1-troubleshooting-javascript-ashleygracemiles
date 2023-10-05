f71da4e7b478fa528d272a7f12119ff41407f869
Ashley Miles <agmiles@nic.edu>
2023-10-04 19:54:13 -0700

    final

---
+++ javascript.js
@@ -0,0 +1,46 @@
+alert("JavaScript is easy");
+
+var john_age = 25;
+var john_gender = 'male';
+var john_location = 'United Kingdom';
+
+console.log(john_age);
+console.log(john_gender);
+console.log(john_location);
+
+var a = "23";
+var b = "Heyyyyyy!";
+var c = true;
+var d = [1, 2, 3];
+var e;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+x = 2;
+y = 3;
+
+z = x + y;
+
+var a = function () {
+    alert('Statement 1');
+    alert('Statement 2');
+}
+
+var f = "Ashley"; // object
+var g = 23; // number
+var h = "I like coffee"; // string, double quote
+
+console.log(f);
+console.log(g);
+console.log(h);
+
+var A = 4;
+var B = 3;
+var C = 5;
+
+console.log(B * A / 2);
+
